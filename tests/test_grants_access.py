e229500b24f614181efaf0aa890b8e526f154741
Artyom Vancyan <44609997+ArtyomVancyan@users.noreply.github.com>
2023-04-05 15:25:01 +0400

    GH-10: Provide request object to the function

--- tests/test_grants_access.py
+++ tests/test_grants_access.py
@@ -3,80 +3,80 @@ from django.test import override_settings
 from django_forbid.access import grants_access
 
 
-def test_access_without_configuration():
+def test_access_without_configuration(request):
     """If no configuration is provided, access is granted everywhere."""
-    assert grants_access("doesnt-matter")
+    assert grants_access(request, "doesnt-matter")
 
 
 @override_settings(WHITELIST_COUNTRIES=["US"], DEBUG=True)
-def test_access_from_localhost_development_mode():
+def test_access_from_localhost_development_mode(request):
     """In development mode, access is granted from localhost."""
-    assert grants_access("127.0.0.1")
-    assert grants_access("localhost")
+    assert grants_access(request, "127.0.0.1")
+    assert grants_access(request, "localhost")
 
 
 @override_settings(WHITELIST_COUNTRIES=["US"])
-def test_access_from_localhost_production_mode():
+def test_access_from_localhost_production_mode(request):
     """In production mode, access is not granted from localhost."""
-    assert not grants_access("127.0.0.1")
-    assert not grants_access("localhost")
+    assert not grants_access(request, "127.0.0.1")
+    assert not grants_access(request, "localhost")
 
 
 @override_settings(WHITELIST_COUNTRIES=["GB"])
-def test_access_from_gb_when_gb_in_countries_whitelist():
+def test_access_from_gb_when_gb_in_countries_whitelist(request):
     """Access is granted from GB when GB is in the counties' whitelist."""
-    assert grants_access("212.102.63.59")
+    assert grants_access(request, "212.102.63.59")
 
 
 @override_settings(WHITELIST_COUNTRIES=["US"])
-def test_access_from_gb_when_gb_not_in_countries_whitelist():
+def test_access_from_gb_when_gb_not_in_countries_whitelist(request):
     """Access is not granted from GB when GB is not in the counties' whitelist."""
-    assert not grants_access("212.102.63.59")
+    assert not grants_access(request, "212.102.63.59")
 
 
 @override_settings(WHITELIST_TERRITORIES=["EU"])
-def test_access_from_gb_when_eu_in_continent_whitelist():
+def test_access_from_gb_when_eu_in_continent_whitelist(request):
     """Access is granted from GB when EU is in the continents' whitelist."""
-    assert grants_access("212.102.63.59")
+    assert grants_access(request, "212.102.63.59")
 
 
 @override_settings(WHITELIST_TERRITORIES=["US"])
-def test_access_from_gb_when_gb_not_in_continent_whitelist():
+def test_access_from_gb_when_gb_not_in_continent_whitelist(request):
     """Access is not granted from GB when EU is not in the continents' whitelist."""
-    assert not grants_access("212.102.63.59")
+    assert not grants_access(request, "212.102.63.59")
 
 
 @override_settings(FORBIDDEN_COUNTRIES=["GB"])
-def test_access_from_gb_when_gb_in_forbidden_countries():
+def test_access_from_gb_when_gb_in_forbidden_countries(request):
     """Access is not granted from GB when GB is in the forbidden list."""
-    assert not grants_access("212.102.63.59")
+    assert not grants_access(request, "212.102.63.59")
 
 
 @override_settings(FORBIDDEN_COUNTRIES=["RU"])
-def test_access_from_gb_when_gb_not_in_forbidden_countries():
+def test_access_from_gb_when_gb_not_in_forbidden_countries(request):
     """Access is granted from GB when GB is not in the forbidden list."""
-    assert grants_access("212.102.63.59")
+    assert grants_access(request, "212.102.63.59")
 
 
 @override_settings(FORBIDDEN_TERRITORIES=["EU"])
-def test_access_from_gb_when_eu_in_forbidden_territories():
+def test_access_from_gb_when_eu_in_forbidden_territories(request):
     """Access is not granted from GB when EU is in the forbidden list."""
-    assert not grants_access("212.102.63.59")
+    assert not grants_access(request, "212.102.63.59")
 
 
 @override_settings(FORBIDDEN_TERRITORIES=["AS"])
-def test_access_from_gb_when_eu_not_in_forbidden_territories():
+def test_access_from_gb_when_eu_not_in_forbidden_territories(request):
     """Access is granted from GB when EU is not in the forbidden list."""
-    assert grants_access("212.102.63.59")
+    assert grants_access(request, "212.102.63.59")
 
 
 @override_settings(WHITELIST_TERRITORIES=["EU"], FORBIDDEN_COUNTRIES=["GB"])
-def test_mix_config_access_from_gb_when_eu_in_whitelist_but_gb_is_forbidden():
+def test_mix_config_access_from_gb_when_eu_in_whitelist_but_gb_is_forbidden(request):
     """Access is not granted from GB when EU is in the whitelist but GB is forbidden."""
-    assert not grants_access("212.102.63.59")
+    assert not grants_access(request, "212.102.63.59")
 
 
 @override_settings(WHITELIST_COUNTRIES=["GB"], FORBIDDEN_COUNTRIES=["GB"])
-def test_mix_config_access_from_gb_when_gb_in_both_lists():
+def test_mix_config_access_from_gb_when_gb_in_both_lists(request):
     """Access is not granted from GB when GB is in both lists."""
-    assert not grants_access("212.102.63.59")
+    assert not grants_access(request, "212.102.63.59")
