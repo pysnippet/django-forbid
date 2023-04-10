cc5ebdee07ded8ccafa6f31a0c506f45732128e3
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-04-10 22:02:46 +0400

    GH-12: Upgrading the old style settings with new style

--- tests/test_grants_access.py
+++ tests/test_grants_access.py
@@ -18,13 +18,13 @@ def test_access_without_configuration():
     assert grants_access(request, LOCALHOST)
 
 
-@override_settings(FORBID_VPN=True)
+@override_settings(DJANGO_FORBID={"OPTIONS": {"VPN": True}})
 def test_access_forbid_vpn():
     """If VPN detection is enabled, access is granted everywhere."""
     assert grants_access(request, LOCALHOST)
 
 
-@override_settings(WHITELIST_COUNTRIES=["US"], DEBUG=True)
+@override_settings(DJANGO_FORBID={"COUNTRIES": ["US"], "OPTIONS": {"ACTION": "PERMIT"}}, DEBUG=True)
 def test_access_from_localhost_development_mode():
     """In development mode, access is granted from localhost."""
     assert grants_access(request, IP_LOCAL1)
@@ -32,7 +32,7 @@ def test_access_from_localhost_development_mode():
     assert grants_access(request, LOCALHOST)
 
 
-@override_settings(WHITELIST_COUNTRIES=["US"])
+@override_settings(DJANGO_FORBID={"COUNTRIES": ["US"], "OPTIONS": {"ACTION": "PERMIT"}})
 def test_access_from_localhost_production_mode():
     """In production mode, access is not granted from localhost."""
     assert not grants_access(request, IP_LOCAL1)
@@ -40,61 +40,49 @@ def test_access_from_localhost_production_mode():
     assert not grants_access(request, LOCALHOST)
 
 
-@override_settings(WHITELIST_COUNTRIES=["GB"])
+@override_settings(DJANGO_FORBID={"COUNTRIES": ["GB"], "OPTIONS": {"ACTION": "PERMIT"}})
 def test_access_from_gb_when_gb_in_countries_whitelist():
     """Access is granted from GB when GB is in the counties' whitelist."""
     assert grants_access(request, IP_LONDON)
 
 
-@override_settings(WHITELIST_COUNTRIES=["US"])
+@override_settings(DJANGO_FORBID={"COUNTRIES": ["US"], "OPTIONS": {"ACTION": "PERMIT"}})
 def test_access_from_gb_when_gb_not_in_countries_whitelist():
     """Access is not granted from GB when GB is not in the counties' whitelist."""
     assert not grants_access(request, IP_LONDON)
 
 
-@override_settings(WHITELIST_TERRITORIES=["EU"])
+@override_settings(DJANGO_FORBID={"TERRITORIES": ["EU"], "OPTIONS": {"ACTION": "PERMIT"}})
 def test_access_from_gb_when_eu_in_continent_whitelist():
     """Access is granted from GB when EU is in the continents' whitelist."""
     assert grants_access(request, IP_LONDON)
 
 
-@override_settings(WHITELIST_TERRITORIES=["US"])
+@override_settings(DJANGO_FORBID={"TERRITORIES": ["US"], "OPTIONS": {"ACTION": "PERMIT"}})
 def test_access_from_gb_when_gb_not_in_continent_whitelist():
     """Access is not granted from GB when EU is not in the continents' whitelist."""
     assert not grants_access(request, IP_LONDON)
 
 
-@override_settings(FORBIDDEN_COUNTRIES=["GB"])
+@override_settings(DJANGO_FORBID={"COUNTRIES": ["GB"], "OPTIONS": {"ACTION": "FORBID"}})
 def test_access_from_gb_when_gb_in_forbidden_countries():
     """Access is not granted from GB when GB is in the forbidden list."""
     assert not grants_access(request, IP_LONDON)
 
 
-@override_settings(FORBIDDEN_COUNTRIES=["RU"])
+@override_settings(DJANGO_FORBID={"COUNTRIES": ["RU"], "OPTIONS": {"ACTION": "FORBID"}})
 def test_access_from_gb_when_gb_not_in_forbidden_countries():
     """Access is granted from GB when GB is not in the forbidden list."""
     assert grants_access(request, IP_LONDON)
 
 
-@override_settings(FORBIDDEN_TERRITORIES=["EU"])
+@override_settings(DJANGO_FORBID={"TERRITORIES": ["EU"], "OPTIONS": {"ACTION": "FORBID"}})
 def test_access_from_gb_when_eu_in_forbidden_territories():
     """Access is not granted from GB when EU is in the forbidden list."""
     assert not grants_access(request, IP_LONDON)
 
 
-@override_settings(FORBIDDEN_TERRITORIES=["AS"])
+@override_settings(DJANGO_FORBID={"TERRITORIES": ["AS"], "OPTIONS": {"ACTION": "FORBID"}})
 def test_access_from_gb_when_eu_not_in_forbidden_territories():
     """Access is granted from GB when EU is not in the forbidden list."""
     assert grants_access(request, IP_LONDON)
-
-
-@override_settings(WHITELIST_TERRITORIES=["EU"], FORBIDDEN_COUNTRIES=["GB"])
-def test_mix_config_access_from_gb_when_eu_in_whitelist_but_gb_is_forbidden():
-    """Access is not granted from GB when EU is in the whitelist but GB is forbidden."""
-    assert not grants_access(request, IP_LONDON)
-
-
-@override_settings(WHITELIST_COUNTRIES=["GB"], FORBIDDEN_COUNTRIES=["GB"])
-def test_mix_config_access_from_gb_when_gb_in_both_lists():
-    """Access is not granted from GB when GB is in both lists."""
-    assert not grants_access(request, IP_LONDON)
