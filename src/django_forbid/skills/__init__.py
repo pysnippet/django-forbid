52ba350d96bc3d868ec2c5a62294db5a356c6ddd
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-05-24 10:43:49 +0400

    GH-21: Extract the access checker logic out

--- src/django_forbid/skills/__init__.py
+++ src/django_forbid/skills/__init__.py
@@ -1,3 +1,5 @@
+import re
+
 from django.conf import settings
 
 
@@ -25,3 +27,38 @@ class Settings:
             return cls._get(item)
         except KeyError:
             return default
+
+
+class Access:
+    """A helper class to check if a user has access to a resource."""
+
+    def __init__(self, attributes):
+        self.attributes = attributes
+
+    @staticmethod
+    def normalize(attribute_type):
+        """Removes the "!" prefix."""
+        return attribute_type[1:]
+
+    @staticmethod
+    def forbidden(attribute_type):
+        """Checks if the type is forbidden."""
+        return attribute_type.startswith("!")
+
+    @classmethod
+    def permitted(cls, attribute_type):
+        """Checks if the type is permitted."""
+        return not cls.forbidden(attribute_type)
+
+    def grants(self, attribute_type):
+        # Creates a regular expression in the following form:
+        # ^(?=PERMITTED_ATTRIBUTES)(?:(?!FORBIDDEN_ATTRIBUTES)\w)+$
+        # where the list of forbidden and permitted attributes are
+        # filtered from the ATTRIBUTES setting by the "!" prefix.
+        permit = r"|".join(filter(self.permitted, self.attributes))
+        forbid = r"|".join(map(self.normalize, filter(self.forbidden, self.attributes)))
+        forbid = r"(?!" + forbid + r")" if forbid else ""
+        regexp = r"^(?=" + permit + r")(?:" + forbid + r"\w)+$"
+
+        # Regexp designed to match the permitted attributes.
+        return re.match(regexp, attribute_type)
