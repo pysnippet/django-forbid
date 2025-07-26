fd02aeab3bc0d545dc286aff4f241114bb16c349
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2025-07-26 13:55:04 +0400

    Fix the wheel path resolvation issue

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@
 pip install build==0.9.0
 
 # build the wheel and install it
-WHEEL_NAME=$(python -m build | grep -Po "django_forbid-.*\.whl" | tail -n 1)
-pip install dist/$WHEEL_NAME
+python -m build && pip install $(ls dist/django_forbid-*.whl)
